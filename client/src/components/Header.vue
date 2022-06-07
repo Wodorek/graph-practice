289776cace32d384e83529367444abbb32c96ef2
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-06-07 20:24:18 +0200

    Create Header.vue

---
+++ client/src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="navbar bg-light mb-4 p-0">
+    <div class="container">
+      <a href="/" class="navbar-brand"
+        ><div class="d-flex">
+          <img src="@/assets/logo.svg" alt="logo" class="mr-2" />
+          <div>GraphQL</div>
+        </div></a
+      >
+    </div>
+  </nav>
+</template>
