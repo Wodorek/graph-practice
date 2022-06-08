2081ca1f372e7e4dd122e71c21941f4c676b7ff3
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-06-08 11:37:01 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './schema/schema';
 import connectDB from './config/db';
@@ -8,8 +9,14 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-connectDB();
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 
+connectDB();
+app.use(cors());
 app.use(
   '/graphql',
   graphqlHTTP({
