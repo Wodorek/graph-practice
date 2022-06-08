02f626f9b11389a2bb9f010ceb7cf4bc9389ad83
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-06-08 11:36:47 +0200

    Added icon component

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,13 @@
 import { createApp, h, provide } from 'vue';
 import { DefaultApolloClient } from '@vue/apollo-composable';
-import App from './App.vue';
-
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 import router from './router';
+import App from './App.vue';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faTrashCan);
 
 const client = new ApolloClient({
   uri: 'http://localhost:3030/graphql',
@@ -19,5 +23,5 @@ const app = createApp({
 });
 
 app.use(router);
-
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
