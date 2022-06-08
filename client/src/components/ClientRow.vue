64ab0c7bce4aefc7193ed46cbd1d0535641cf89e
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-06-08 11:36:26 +0200

    Create ClientRow.vue

---
+++ client/src/components/ClientRow.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import type { Client } from '../../types';
+
+const props = defineProps({
+  client: {
+    required: true,
+    type: Object as PropType<Client>,
+  },
+});
+</script>
+
+<template>
+  <tr>
+    <td>{{ props.client.name }}</td>
+    <td>{{ props.client.email }}</td>
+    <td>{{ props.client.phone }}</td>
+    <td>
+      <button class="btn btn-danger btn-sm">
+        <font-awesome-icon
+          size="1"
+          :icon="['fas', 'trash-can']"
+        ></font-awesome-icon>
+      </button>
+    </td>
+  </tr>
+</template>
+
+<style></style>
