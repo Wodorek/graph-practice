8b1d4282b792879d464427b03c8fec5e994927d9
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-06-08 11:36:21 +0200

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <Header />
+  <router-view></router-view>
 </template>
 
 <style>
